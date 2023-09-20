856e2c37058bbb2bc1a8c6bc33177e4df789f9d9
diamondburned <diamond@arikawa-hi.me>
2023-09-19 21:20:24 -0700

    Add main.py boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, world!")
+#!/usr/bin/env python3
+import os
+
+from dotenv import load_dotenv
+from bot import bot
+
+
+# Load the .env file, if it exists, so that os.getenv() can read the environment
+# variable in it.
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+if DISCORD_TOKEN is None:
+    raise EnvironmentError("'DISCORD_TOKEN' is not set!")
+
+# Run the bot with the token
+bot.run(DISCORD_TOKEN)
